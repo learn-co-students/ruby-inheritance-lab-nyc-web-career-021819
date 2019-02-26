a609b58f1afeeed1c84a2f573a76e26cd6716ebd
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-02-25 22:07:50 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,24 @@
-class Student 
+require 'pry'
+
+# attr_accessor :knowledge
+
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(lesson)
+    # binding.pry
+    self.knowledge << lesson
+  end
+
+  def knowledge=(stirng)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
